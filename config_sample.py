a5e967bc9e78417246d8ae053443372091e27302
avishaigonen123 <avishaigonen@gmail.com>
2024-06-28 16:55:04 +0300

    first commit, added skelton

---
+++ config_sample.py
@@ -0,0 +1,15 @@
+# SMS staff
+
+ACCOUNT_SID = '<account_sid>'
+AUTH_TOKEN = '<auto_token>'
+
+TWILIO_NUMBER = '<twilio_number>' 
+
+RECIPIENT_NUMBER = '<recipient_number>' 
+
+# Whatsapp staff
+
+API_URL = '<api_url>'
+ID_INSTANCE = '<instance_id>'
+API_TOKEN_INSTANCE = '<api_token_instance>'
+CHAT_ID = '<chat_id>'
